82a811940fb36398889fada9f0dd60610f799fba
Fred Jabbari <ffjabbari@gmail.com>
2015-01-10 19:05:44 -0600

    added javadoc

--- java/monthlyExpense2/src/main/java/com/savvis/hr/report/Entity.java
+++ java/monthlyExpense2/src/main/java/com/savvis/hr/report/Entity.java
@@ -4,35 +4,85 @@ import java.util.List;
 
 public interface Entity {
 
+	/**
+	 * @return
+	 */
 	public String getName();
 
+	/**
+	 * @param name
+	 */
 	public void setName(String name);
 
+	/**
+	 * @return
+	 */
 	public double getAllocation();
 
+	/**
+	 * @param allocation
+	 */
 	public void setAllocation(double allocation);
 
+	/**
+	 * @return
+	 */
 	public String toString();
 
+	/**
+	 * @return
+	 */
 	public List<Entity> report();
 
+	/**
+	 * @return
+	 */
 	public List<Entity> getExpenseDetail();
 
+	/**
+	 * @return
+	 */
 	public List<Entity> getExpenseSummary();
 
+	/**
+	 * @param entity
+	 */
 	public void add(Entity entity);
 
+	/**
+	 * @param currNode
+	 * @param newNode
+	 */
 	public void add(Entity currNode, Entity newNode);
 
+	/**
+	 * @param entity
+	 */
 	public void remove(Entity entity);
 
+	/**
+	 * @param i
+	 * @return
+	 */
 	public Entity getChild(int i);
 
+	/**
+	 * @return
+	 */
 	public String getType();
 
+	/**
+	 * @param lstEntity
+	 */
 	public void printChildren(List<Entity> lstEntity);
 
+	/**
+	 * @return
+	 */
 	public double calculateTotalAllocation();
 
+	/**
+	 * @return
+	 */
 	public int calculateTotalDepth();
 }
